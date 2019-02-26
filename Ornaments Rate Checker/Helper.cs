16418360d611197af34a9cb4270eaa4fcaf8a752
harshalghule1896 <harshalghule07@gmail.com>
2019-02-26 21:49:07 +0530

    first commit

---
+++ Ornaments Rate Checker/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornaments_Rate_Checker
+{
+    class Rate
+    {
+        public Rate()
+        {
+
+        }
+
+        public String GoldRate(String Url)
+        {
+            String sRet = null;
+            WebClient web = new WebClient();
+            String html = web.DownloadString(Url);
+            String data = html.Substring(html.LastIndexOf("<td>10 gram</td>"), 150);
+
+            data = data.Replace("<td>", "");
+            data = data.Replace("</td>", "");
+            data = data.Replace(" ", "");
+            data = data.Replace("&#8377;", "₹");
+            String[] arr = data.Split('\n');
+            sRet = arr[0] +" Today " + arr[1] + " Yesterday " + arr[2];
+
+            return sRet;
+        }
+
+        public String SilverRate(String Url)
+        {
+            String sRet = null;
+            WebClient web = new WebClient();
+            String html = web.DownloadString(Url);
+            String data = html.Substring(html.LastIndexOf("<td>1 Kg</td>"), 150);
+
+            data = data.Replace("<td>", "");
+            data = data.Replace("</td>", "");
+            data = data.Replace(" ", "");
+            data = data.Replace("&#8377;", "₹");
+            String[] arr = data.Split('\n');
+            sRet = arr[0] + " Today " + arr[1] + " Yesterday " + arr[2];
+
+            return sRet;
+        }
+    }
+}
---
+++ Ornaments Rate Checker/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ornaments_Rate_Checker
+{
+    public partial class MainWindow : Window
+    {
+        private String GoldUrl;
+        private String SilverUrl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GoldUrl = "https://www.goodreturns.in/gold-rates/pune.html";
+            SilverUrl = "https://www.goodreturns.in/silver-rates/pune.html";
+        }
+
+          private void Listener(object sender, RoutedEventArgs e)
+          {
+            Button btn = (Button)sender;
+
+            try
+            {
+                switch (btn.Uid)
+                {
+                    case "Chk":
+                        Rate obj = new Rate();
+                        GoldRate.Text = obj.GoldRate(GoldUrl);
+                        SilverRate.Text = obj.SilverRate(SilverUrl);
+                        break;
+
+                    case "Rst":
+                        GoldRate.Text = "";
+                        SilverRate.Text = "";
+                        break;
+                }
+            }catch (Exception)
+            {
+                MessageBox.Show("Connection not avalable");
+            }
+        }
+
+          
+        
+    }
+}
